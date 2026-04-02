cb8ccf2ba81bacb2755fab1ded503cc27963332d
kuuki1 <phamtin010900@gmail.com>
2026-04-02 20:18:31 +0700

    first commit

---
+++ backend/src/test/java/com/web_calling/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_calling.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
